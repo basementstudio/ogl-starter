ac5397b31388d08ebe0ad9a48d8c7e02a21e4794
Matias Gonzalez Fernandez <matiasngf@hotmail.com>
2024-09-19 16:50:13 -0300

    first commit

---
+++ postcss.config.mjs
@@ -0,0 +1,10 @@
+/** @type {import('postcss-load-config').Config} */
+const config = {
+  plugins: [
+    'postcss-hover-media-feature',
+    'postcss-nested',
+    'tailwindcss',
+  ]
+};
+
+export default config;
