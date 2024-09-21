630c566c91912ad23c47345997d6db0ae85e1173
Nacho Mandagaran <ignaciofmandagaran@gmail.com>
2024-09-21 19:49:27 -0300

    chore: add app-hooks in layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,8 @@ import "./globals.css"
 import type { Metadata } from "next"
 import { Inter } from "next/font/google"
 
+import { AppHooks } from "./app-hooks"
+
 const inter = Inter({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
@@ -17,6 +19,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <AppHooks />
       <body className={inter.className}>{children}</body>
     </html>
   )
